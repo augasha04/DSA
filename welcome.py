81478b08aec534520aa7f798af7e841d0c7a63c4
augasha04 <augustinemwangi04@gmail.com>
2023-07-18 13:50:28 +0300

    done with welcome

---
+++ welcome.py
@@ -0,0 +1,26 @@
+def greet(language):
+    greetings = {
+        'english': 'Welcome',
+        'czech': 'Vitejte',
+        'danish': 'Velkomst',
+        'dutch': 'Welkom',
+        'estonian': 'Tere tulemast',
+        'finnish': 'Tervetuloa',
+        'flemish': 'Welgekomen',
+        'french': 'Bienvenue',
+        'german': 'Willkommen',
+        'irish': 'Failte',
+        'italian': 'Benvenuto',
+        'latvian': 'Gaidits',
+        'lithuanian': 'Laukiamas',
+        'polish': 'Witamy',
+        'spanish': 'Bienvenido',
+        'swedish': 'Valkommen',
+        'welsh': 'Croeso'
+    }
+    
+    if language in greetings:
+        return greetings[language]
+    else:
+        return greetings['english']
+#         print("Welcome")

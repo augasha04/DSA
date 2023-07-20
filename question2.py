1212a55893015619a7367e70d58fa87ead31b588
augasha04 <augustinemwangi04@gmail.com>
2023-07-20 09:23:04 +0300

    finished creating the questions on the files

---
+++ question2.py
@@ -0,0 +1,23 @@
+# Given an array of integers, 
+# filter them according to a simple condition and return an aggregate of the results.
+# Task description
+# Write a function:
+
+# def solution(A)
+
+# that, given an array A consisting of N integers, 
+# returns the maximum among all integers which are multiples of 4.
+
+# For example, given array A as follows:
+
+# [-6, -91, 1011, -100, 84, -22, 0, 1, 473]
+
+# the function should return 84.
+
+# Assume that:
+
+# N is an integer within the range [1..1,000];
+# each element of array A is an integer within the range [−10,000..10,000];
+# there is at least one element in array A which satisfies the condition in the task statement.
+# In your solution, focus on correctness. 
+# The performance of your solution will not be the focus of the assessment.
